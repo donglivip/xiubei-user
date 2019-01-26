ed45cc98d7286b4909c1e35d0e6222822607c0be
dongxiaoli <1076454380@qq.com>
2019-01-26 15:57:59 +0800

    bug修改

--- js/url.js
+++ js/url.js
@@ -1,1 +1,2 @@
-var url_url = localStorage.setItem('http_url',"http://www.taoge110.com");
+//var url_url = localStorage.setItem('http_url',"http://www.taoge110.com");
+var url_url = localStorage.setItem('http_url',"http://www.bmwnxg.com");
